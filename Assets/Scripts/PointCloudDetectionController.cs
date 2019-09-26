fa84e908bb3e8ee0040294ce8669f442f736513f
Maria Judova <maja.judova@gmail.com>
2019-09-27 00:03:06 +0200

    deer added
    -added deer (3D model, texture imported, material and box collider created)
    -custom icon for app added
    -slider for scale and rotate made bigger to allow more comfortable control

---
+++ Assets/Scripts/PointCloudDetectionController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+/// <summary>
+/// This example demonstrates how to toggle plane detection,
+/// and also hide or show the existing planes.
+/// </summary>
+[RequireComponent(typeof(ARPointCloudManager))]
+public class PointCloudDetectionController : MonoBehaviour
+{
+    [Tooltip("The UI Text element used to display plane detection messages.")]
+    [SerializeField]
+    Text m_TogglePointDetectionText;
+
+    /// <summary>
+    /// The UI Text element used to display plane detection messages.
+    /// </summary>
+    public Text togglePointDetectionText
+    {
+        get { return m_TogglePointDetectionText; }
+        set { m_TogglePointDetectionText = value; }
+    }
+
+    /// <summary>
+    /// Toggles plane detection and the visualization of the planes.
+    /// </summary>
+    public void TogglePointDetection()
+    {
+        m_ARPointCloudManager.enabled = !m_ARPointCloudManager.enabled;
+
+        string pointDetectionMessage = "";
+        if (m_ARPointCloudManager.enabled)
+        {
+            pointDetectionMessage = "Disable Point Detection and Hide Existing";
+            SetAllPointsActive(true);
+        }
+        else
+        {
+            pointDetectionMessage = "Enable Point Detection and Show Existing";
+            SetAllPointsActive(false);
+        }
+
+        if (togglePointDetectionText != null)
+            togglePointDetectionText.text = pointDetectionMessage;
+    }
+
+    /// <summary>
+    /// Iterates over all the existing planes and activates
+    /// or deactivates their <c>GameObject</c>s'.
+    /// </summary>
+    /// <param name="value">Each planes' GameObject is SetActive with this value.</param>
+    void SetAllPointsActive(bool value)
+    {
+        foreach (var point in m_ARPointCloudManager.trackables)
+            point.gameObject.SetActive(value);
+    }
+
+    void Awake()
+    {
+        m_ARPointCloudManager = GetComponent<ARPointCloudManager>();
+    }
+
+    ARPointCloudManager m_ARPointCloudManager;
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# AR3f
-Augmented Reality application for the exhibition on the 3rd floor at YCAM.
+# AR3f-v09
+Augmented Reality application for the exhibition on the 3rd floor at YCAM. <br/>
+
+**Commit** <br/>
+-added deer (3D model, texture imported, material and box collider created) <br/>
+-custom icon for app added <br/>
+-slider for scale and rotate made bigger to allow more comfortable control <br/> 
+
+**Instructions for user** <br/>
+1. point your camera at the floor and scan around <br/>
+2. tap on the screen to place 3D object <br/>
+3. scale 3D object using slider <br/>
+4. rotate 3D object using slider <br/>
+5. press the button AllSet to lock the position and hide sliders, text, plane and feature points <br/>
+*see folder “Preview” <br/> 
